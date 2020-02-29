86b3a74e5974f14041610eeb4f37ba151d3dbb88
ganargatul <ganar.gatul@gmail.com>
2020-02-29 20:17:37 +0700

    Update to RN 0.60

--- android/app/src/main/java/com/smktelkom/hajiku/MainApplication.java
+++ android/app/src/main/java/com/smktelkom/hajiku/MainApplication.java
@@ -1,20 +1,22 @@
 package com.smktelkom.hajiku;
 
+import android.util.Log;
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import cl.json.RNSharePackage;
 import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
+import com.horcrux.svg.SvgPackage;
+import cl.json.RNSharePackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.horcrux.svg.SvgPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -27,15 +29,12 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNSharePackage(),
-            new ReactNativeYouTube(),
-            new ReactVideoPackage(),
-            new RNFetchBlobPackage(),
-            new SvgPackage(),
-            new RNI18nPackage()
-      );
+     
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      return packages;
     }
 
     @Override
